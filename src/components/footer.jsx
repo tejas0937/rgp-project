8929da7cd40353e0520d4969d44964d00b42b725
tejas0937 <161461371+tejas0937@users.noreply.github.com>
2025-07-05 22:34:28 +0530

    Final Footer chnages

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
+        <footer>
         <div className='footer flex '>
             <div className="box-1 w-1/3 relative ">
                 <div className="logo flex-col items-center">
@@ -18,35 +19,36 @@ const Footer = () => {
                 </div>
             </div>
             <div className="box-2 w-1/3">
-                <h2 className="font-bold">Get in touch</h2>
-                <ul className="space-y-2">
-                    <li className="transition-colors duration-200 hover:text-blue-600">
-                        <span className="font-semibold">Address:</span> RGP Group , Behind Surin Auto Company, Village Bhamboli, Chakan Tal - Khed, Pune - 410505,Maharashtra, India
+                <h2 className="font-bold text-3xl pb-8">Get in touch</h2>
+                <ul className="space-y-4">
+                    <li className="transition-colors duration-200 hover:text-amber-800 flex ">
+                        <img src='/location-icon.png' alt="Location icon" className='img-loc' /><p className='pl-3'>RGP Group , Behind Surin Auto Company, Village Bhamboli, Chakan Tal - Khed, Pune - 410505,Maharashtra, India</p>
                     </li>
-                    <li className="transition-colors duration-200 hover:text-blue-600">
-                        <span className="font-semibold">Email:</span> <Link to="mailto:rgppacktech@gmail.com" className="hover:underline">rgppacktech@gmail.com</Link>
+                    <li className="transition-colors duration-200 hover:text-amber-800 flex items-center">
+                        <img src='/mail-icon.png' alt="Location icon " /> <Link to="mailto:rgppacktech@gmail.com" className="hover:underline pl-3">rgppacktech@gmail.com</Link>
                     </li>
-                    <li className="transition-colors duration-200 hover:text-blue-600">
-                        <span className="font-semibold">Contact No:</span> <Link to="tel:7083477803" className="hover:underline">7083477803 / 9226771188</Link>
+                    <li className="transition-colors duration-200 hover:text-amber-800 flex items-center">
+                        <img src='/call-icon.png' alt="Location icon " /><Link to="tel:7083477803" className="hover:underline pl-3">7083477803 / 9226771188</Link>
                     </li>
                 </ul>
             </div>
-            <div className="box-3 w-1/3 ml-7 relative">
-                <h2 className="font-bold ">Links</h2>
+            <div className="box-3 w-1/3  relative">
+                <h2 className="font-bold text-3xl pb-5 ">Links</h2>
                 <ul className="space-y-1">
-                    <li><Link to="/" className="hover:underline">Home</Link></li>
+                    <li><Link to="/" className="hover:underline ">Home</Link></li>
                     <li><Link to="/products" className="hover:underline">Products</Link></li>
                     <li><Link to="/about" className="hover:underline">About us</Link></li>
                     <li><Link to="/contact" className="hover:underline">Contact Us</Link></li>
                 </ul>
                 <div className="wtsp">
-                    <Link to="https://wa.me/917083477803" target="_blank" rel="noopener noreferrer" className="flex items-center w-70 bg-green-500 rounded-[50px] text-white rounded hover:bg-green-600 transition-colors duration-200" >
+                    <Link to="https://wa.me/917083477803" target="_blank" rel="noopener noreferrer" className="flex items-center w-70 bg-green-500 rounded-[50px] text-white hover:bg-green-800 transition-colors duration-500" >
                         <img src="/wtsp.svg" alt="whatsapp logo" />
                         <h1>Chat with us on WhatsApp</h1>
                     </Link>
                 </div>
             </div>
-        </div>)
+        </div>
+        </footer>)
 }
 
 export default Footer
