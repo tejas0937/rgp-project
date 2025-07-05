bbef6b831230fc405d80a26873f9d26a76698aba
RushiShivale <rushikeshshivale38@gmail.com>
2025-07-05 16:00:13 +0530

    a href

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
 
 
 function Navbar() {
@@ -14,10 +13,10 @@ function Navbar() {
     <div className='bg-red-50 text-black m-2 flex justify-between items-center rounded-xl'>
         <h1 className='w-full m-4'><img src="/rgplogo-black.png" alt="Logo" className='h-25' /></h1>
         <ul className='justify-start items-center gap-10 text-lg mr-6 hidden md:flex'>
-            <link to='/'>Home</link>
-            <link to='/product'>Product</link>
-            <link to='/about'>About</link>
-            <link to='/contact'>Contact</link>
+            <a href='/'>Home</a>
+            <a href='/product'>Product</a>
+            <a href='/about'>About</a>
+            <a href='/contact'>Contact</a>
         </ul>
 
         <div onClick={handleNav} className='block md:hidden'>
@@ -31,10 +30,10 @@ function Navbar() {
                 <h1 className='text-2xl font-semibold'>RGPGROUP.IN <p className='text-xs'>"Our Work is our Identity"</p></h1>
             </div>
             <ul className='uppercase p-4'>
-                <link to='/' className='p-4 border-b border-grey-100'>Home</link>
-                <link to='/product' className='p-4 border-b border-grey-100'>Product</link>
-                <link to='/about' className='p-4 border-b border-grey-100'>About</link>
-                <link to='/contact' className='p-4'>Contact</link>
+                <a href='/' className='p-4 border-b border-grey-100'>Home</a>
+                <a href='/product' className='p-4 border-b border-grey-100'>Product</a>
+                <a href='/about' className='p-4 border-b border-grey-100'>About</a>
+                <a href='/contact' className='p-4'>Contact</a>
             </ul>
         </div>
     </div>
