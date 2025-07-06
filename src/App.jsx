24f2a9ed22b1b6f6f86ce109cae38fa2e620c17a
tejas0937 <161461371+tejas0937@users.noreply.github.com>
2025-07-06 13:57:52 +0530

    Changed routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,24 @@ import Footer from "./components/footer";
 import About from "./components/about";
 import Products from "./components/products";
 import Contact from "./components/contact";
-import Hero from "./components/hero";
 import SmoothScroll from "./components/smooth-scroll";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
+    
     <SmoothScroll />
     <div className="main relative">
       <nav className="sticky z-50 top-0">
         <Navbar />
       </nav>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <div className="main w-full relative">
-        <Hero />
-        </div>
         <div className="head w-full h-screen"></div>
         <Footer />
       </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from "./hero";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="main w-full relative">
+        <Hero />
+        </div>
+    </div>
+  )
+}
+
+export default Home
