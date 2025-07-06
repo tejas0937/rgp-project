e9abf89c71d97526139dc113ab714ebd60b0e409
RushiShivale <rushikeshshivale38@gmail.com>
2025-07-07 00:26:55 +0530

    contact UI done

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,11 +1,14 @@
 // src/pages/Contact.jsx
 import React from "react";
+import './contact.css';
+import { Link } from "react-router-dom";
 
+// gst qms google map
 const Contact = () => {
   return (
     <div className="w-full bg-[#f5f5f5] py-16 px-6 md:px-24">
       {/* Header */}
-      <h2 className="text-sm font-semibold uppercase text-gray-800 mb-2">
+      <h2 className="text-md font-semibold uppercase text-orange-500 mb-2">
         Contact Us
       </h2>
       <h1 className="text-5xl font-extrabold text-gray-900 mb-10">
@@ -13,7 +16,7 @@ const Contact = () => {
       </h1>
 
       {/* Main Content: Info + Form */}
-      <div className="grid md:grid-cols-2 gap-12">
+      <div className="grid md:grid-cols-2 gap-16">
         {/* Left Section: Info */}
         <div className="space-y-10 text-gray-700 text-lg">
           <p>
@@ -31,76 +34,83 @@ const Contact = () => {
             <div>
               <h3 className="text-lg font-bold uppercase text-gray-800 mb-2">Address</h3>
               <p>
-                - Gat no.227, Bhamboli Village,<br />
-                Chakan MIDC phase-2, Pune-410501
+                Gat No : , Behind Surin Auto Company <br />
+                Village : Bhamboli, Chakan <br />
+                Tal : Khed, Dist : Pune <br />
+                State : Maharashtra <br />
+                Pin: 410501. <br />
+                INDIA
               </p>
-              <p className="mt-2">
-                - Gat no.155, Koregaon Khurd,<br />
-                Chakan, Taluka-Khed, Pune-410501
-              </p>
+
             </div>
           </div>
 
           {/* Contact */}
           <div className="flex gap-4">
             <div className="bg-[#fffaf0] p-3 rounded h-fit">
-              <img src="/call-icon.png" alt="Phone" className="w-6 h-6" />
+              <img src="/wtsp.svg" alt="Phone" className="w-6 h-6" />
             </div>
             <div>
               <h3 className="text-lg font-bold uppercase text-gray-800 mb-2">Contact Details</h3>
-              <p>
-                <a
-                  href="mailto:rgpunit2@gmail.com"
-                  className="text-blue-600 underline"
-                >
-                  rgpunit2@gmail.com
-                </a>
-              </p>
-              <p className="text-xl font-bold mt-1 text-gray-900">+91 7066 727 272</p>
+              <ul>
+                <li className="transition-colors duration-200 hover:text-amber-800 flex items-center w-6 h-6 mb-2">
+                  <img src='/mail-icon.png' alt="Location icon " /> <Link to="mailto:rgppacktech@gmail.com" className="hover:underline pl-3">rgppacktech@gmail.com</Link>
+                </li>
+                <li className="transition-colors duration-200 hover:text-amber-800 flex items-center whitespace-nowrap w-6 h-6">
+                  <img src='/call-icon.png' alt="Location icon " /><Link to="tel:7083477803" className="hover:underline pl-3">+91 9881340251</Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
 
-        {/* Right Section: Form */}
-        <form className="bg-white shadow-md rounded-lg p-8 space-y-6">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Name</label>
-            <input
-              type="text"
-              className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
-              placeholder="Your name"
-              required
-            />
-          </div>
-
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Email</label>
-            <input
-              type="email"
-              className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
-              placeholder="Your email"
-              required
-            />
-          </div>
-
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Message</label>
-            <textarea
-              rows="5"
-              className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
-              placeholder="Your message"
-              required
-            ></textarea>
-          </div>
-
-          <button
-            type="submit"
-            className="bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-600 transition"
-          >
-            Send Message
-          </button>
-        </form>
+        {/* Right Section: map*/}
+        <div className="loc-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3777.253777910555!2d73.7773357093428!3d18.786840382285522!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b444de5039ab%3A0xee3d9503fb63bc7d!2sRGP%20Group!5e0!3m2!1sen!2sin!4v1751800429185!5m2!1sen!2sin" width="600" height="450" allowFullScreen="" referrerPolicy="no-referrer-when-downgrade"></iframe></div>
       </div>
+
+      {/* Contact Form */}
+
+      <form className="bg-white shadow-md rounded-lg p-8 ">
+        {/* Header */}
+      <h2 className="text-md font-semibold uppercase text-orange-500 mb-2">
+        Feedback Form
+      </h2>
+        <div className="txt_field">  
+          <input
+            type="text"
+            required
+          />
+          <label>Name</label>
+          
+        </div>
+
+        <div className="txt_field">          
+          <input
+            type="email"
+            placeholder=" "
+            required
+          />
+          <label>Email</label>
+        </div>
+
+        <div className="txt_field">
+          <textarea
+            rows="5"
+            name="message"
+            placeholder=" "
+            required
+
+          ></textarea>
+          <label>Message</label>
+        </div>
+
+        <button
+          type="submit"
+          className="submit"
+        >
+          Send Message
+        </button>
+      </form>
     </div>
   );
 };
