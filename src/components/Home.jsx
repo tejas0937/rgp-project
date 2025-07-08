c877f0ca9d5ae61fa3388c8feb5a5411ec935318
tejas0937 <161461371+tejas0937@users.noreply.github.com>
2025-07-09 01:10:51 +0530

    final theme changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
         <Hero />
       </div>
 
-      <div className="video-infra bg-blue-100 w-full h-2/4 mt-0 flex items-center justify-center flex-col">
+      <div className="video-infra bg-white text-blue-950  w-full h-2/4 mt-0 flex items-center justify-center flex-col">
         <h1 className='text-6xl font-bold m-20text-amber-900 m-16 bg-[linear-gradient(360deg,_rgba(112,210,255,1)_0%,_rgba(17,81,130,1)_100%)] bg-center w-4xl text-center p-10 rounded-3xl text-black'>Company Infrastructure</h1>
         <video src="/bg/bgvideo-rgp.mp4" controls muted autoPlay loop className='w-7/8 mb-20 max-md:mb-5 max-md:rounded-xs max-mt-5 rounded-2xl shadow-navbar shadow-[0_4px_12px_-4px_rgba(0,0,0,0.75)]'></video>
         <p className='infraloc-info flex justify-center m-10 gap-8 w-5/6 text-black max-md:m-1'>
@@ -45,7 +45,7 @@ const Home = () => {
           </div>
           <Link to="/about">
             <button
-              className='text-blue-950 bg-white w-60 cursor-pointer mt-6 ml-15 p-5 text-2xl font-bold rounded-2xl transition duration-200 ease-in-out hover:bg-blue-950 hover:text-white hover:shadow-lg'
+              className='  border-2 border-white  bg-white w-60 cursor-pointer mt-6 ml-15 p-5 text-2xl font-bold rounded-2xl transition duration-200 ease-in-out hover:bg-white text-blue-950  hover:text-white hover:shadow-lg'
             >
               View More Info
             </button>
@@ -53,7 +53,7 @@ const Home = () => {
         </div>
 
       </div>
-      <div className="join-info flex w-full h-150 bg-blue-100 my-40 shadow-[0_4px_12px_-4px_rgba(0,0,0,0.55)]">
+      <div className="join-info flex w-full h-150 bg-white text-blue-950  my-40 shadow-[0_4px_12px_-4px_rgba(0,0,0,0.55)]">
         <div className="join-img">
           <img src="/plant/team-rgp.jpg
           " alt="" className='w-9/8 h-full' />
@@ -73,42 +73,42 @@ const Home = () => {
             </ul></p>
           </div></div>
       </div>
-      <div className="home-pro flex flex-col w-full  bg-blue-100 items-center py-20 max-md:py-7">
-        <h1 id="our-products"className='text-6xl font-bold text-amber-900 text-center '>Our Products</h1>
+      <div className="home-pro flex flex-col w-full  bg-white text-blue-950 items-center py-20 max-md:py-7">
+        <h1 id="our-products"className='text-6xl font-bold text-blue-950 text-center '>Our Products</h1>
         <div className="hp-info flex flex-wrap w-3/4 justify-center gap-14 mt-10">
         <Link to="/products#corrugated" className="hp-ind"><div className='flex flex-col items-center' >
           <img src="/Products/cor-heavyduty.jpg" alt="" className='w-7/8' />
-          <h1> Corrogated Packaging</h1> <p className='bg-blue-100 w-50 p-1 m-2 rounded text-blue-900 border-1 border-black'>View Products</p>
+          <h1> Corrogated Packaging</h1> <p className='bg-white text-blue-950 w-50 p-1 m-2 rounded  border-1 border-black'>View Products</p>
         </div></Link>
         <Link to="/products#wooden"className="hp-ind"><div className='flex flex-col items-center' >
           <img src="/Products/wooden-pinejunglewood.jpg" alt="" className='w-7/8' />
-          <h1> Wooden <br></br>Packaging</h1><p className='bg-blue-100 w-50 p-1 m-2 rounded text-blue-900 border-1 border-black'>View Products</p>
+          <h1> Wooden <br></br>Packaging</h1><p className='bg-white text-blue-950  w-50 p-1 m-2 rounded   border-1 border-black'>View Products</p>
         </div></Link>
         <Link to="/products#protective" className="hp-ind "><div className='flex flex-col items-center'>
           <img src="/Products/prot-edge protectors.jpg" alt="" className='w-7/8' />
-          <h1> Protective Accessories</h1><p className='bg-blue-100 w-50 p-1 m-2 rounded text-blue-900 border-1 border-black'>View Products</p>
+          <h1> Protective Accessories</h1><p className='bg-white text-blue-950  w-50 p-1 m-2 rounded   border-1 border-black'>View Products</p>
         </div></Link>
         <Link to="/products#custom"className="hp-ind "><div  className='flex flex-col items-center'>
           <img src="/Products/cust-assemblywise.jpg" alt="" className='w-7/8' />
-          <h1> Custom Packaging Solutions</h1><p className='bg-blue-100 w-50 p-1 m-2 rounded text-blue-900 border-1 border-black'>View Products</p>
+          <h1> Custom Packaging Solutions</h1><p className='bg-white text-blue-950  w-50 p-1 m-2 rounded   border-1 border-black'>View Products</p>
         </div></Link>
         <Link to="/products#our-work"className="hp-ind "><div className='flex flex-col items-center' >
           <img src="/our-work/recentwork.jpg" alt="" className='w-7/8' />
-          <h1> View Our Recent Work</h1><p className='bg-blue-100 w-50 p-1 m-2 rounded text-blue-900 border-1 border-black'>View Recent Work</p>
+          <h1> View Our Recent Work</h1><p className='bg-white text-blue-950  w-50 p-1 m-2 rounded  border-1 border-black'>View Recent Work</p>
         </div></Link></div>
       </div>
       <div className="vision-board flex ">
 
-        <div className="vision"> <h1 className='text-5xl font-bold pb-4 pt-5 text-blue-950'>Our Team</h1><p className='team-info text-2xl'>We have always been connected with experienced professionals since our inception, that’s why we have a pool of professionals which possess outstanding industry knowledge. The professionals appointed by us are well aware of the latest market trends of packaging industry. The professionals of our firm have complete knowledge of advanced machines and technologies owing to which we are able to develop remarkable products. Also, skills enhancement classes and seminars are organized by us at regular intervals to keep our workforce abreast with latest technologies and market dynamics.
+        <div className="vision"> <h1 className='text-5xl font-bold pb-4 pt-5 text-blue-950 '>Our Team</h1><p className='team-info text-2xl'>We have always been connected with experienced professionals since our inception, that’s why we have a pool of professionals which possess outstanding industry knowledge. The professionals appointed by us are well aware of the latest market trends of packaging industry. The professionals of our firm have complete knowledge of advanced machines and technologies owing to which we are able to develop remarkable products. Also, skills enhancement classes and seminars are organized by us at regular intervals to keep our workforce abreast with latest technologies and market dynamics.
         </p>
         </div>
         <img src="/bg/emp-2.JPG" alt="background for plant photo" />
 
       </div>
-      <div className="client-info flex w-full h-150 bg-gradient-to-r from-blue-100 to-blue-200 my-40  items-center flex-col shadow-[0_4px_12px_-4px_rgba(0,0,0,0.55)]">
-        <h1 className='text-6xl font-bold text-blue-950 text-center w-7/8 my-20 border-b-blue -950 border-b-2 pb-10'>Our Clients</h1>
+      <div className="client-info flex w-full h-150 bg-gradient-to-r  text-blue-950  to-white   my-40  items-center flex-col shadow-[0_4px_12px_-4px_rgba(0,0,0,0.55)]">
+        <h1 className='text-6xl font-bold  text-blue-950  text-center w-7/8 my-20  white  0 border-b-2 pb-10'>Our Clients</h1>
         <div className="marquee w-7/8">
-        <Marquee autoFill pauseOnClick speed={40}> 
+        <Marquee autoFill pauseOnClick speed={30}> 
               <img src="/clients/client1.png" width="65px" alt="" className='bg-amber-white ml-5' />
               <img src="/clients/client2.png" width="65px" alt="" className='bg-amber-white ml-5' />
               <img src="/clients/client3.png" width="65px" alt="" className='bg-amber-white ml-5' />
